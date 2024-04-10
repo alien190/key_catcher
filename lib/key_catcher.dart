167bca8290376e9375b0f98e9ad722d92a29d28a
alien <alien1900@bk.ru>
2024-04-10 11:10:52 +0700

    Adds platform check

--- lib/key_catcher.dart
+++ lib/key_catcher.dart
@@ -1,23 +1,27 @@
-
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
-
 
 abstract class KeyCatcher {
   static const _methodChannel = MethodChannel('key_catcher_method');
   static const _eventChannel = EventChannel('key_catcher_event');
   static StreamSubscription? _streamSubscription;
 
- static Future<void> init(VoidCallback callback) async {
-    await _methodChannel.invokeMethod('init');
-    _streamSubscription?.cancel();
-    _streamSubscription = _eventChannel.receiveBroadcastStream().listen((_) => callback());
+  static Future<void> init(VoidCallback callback) async {
+    if (Platform.isIOS) {
+      await _methodChannel.invokeMethod('init');
+      _streamSubscription?.cancel();
+      _streamSubscription =
+          _eventChannel.receiveBroadcastStream().listen((_) => callback());
+    }
   }
 
   static Future<void> dispose() async {
-    await _methodChannel.invokeMethod('dispose');
-    _streamSubscription?.cancel();
-    _streamSubscription = null;
+    if (Platform.isIOS) {
+      await _methodChannel.invokeMethod('dispose');
+      _streamSubscription?.cancel();
+      _streamSubscription = null;
+    }
   }
 }
