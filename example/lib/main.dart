86ab0a2278f4efe01103f741fcbc73760993e452
n.ivanov <n.ivanov@inspector-cloud.com>
2024-04-10 10:54:49 +0700

    Implemented

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,51 +1,12 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:key_catcher/key_catcher.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _keyCatcherPlugin = KeyCatcher();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _keyCatcherPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +16,21 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FilledButton(
+                onPressed: () {
+                  KeyCatcher.init(() => print('key!'));
+                },
+                child: Text('start'),
+              ),
+              FilledButton(
+                onPressed: () => KeyCatcher.dispose(),
+                child: Text('stop'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- ios/Classes/KeyCatcherPlugin.swift
+++ ios/Classes/KeyCatcherPlugin.swift
@@ -1,19 +1,105 @@
 import Flutter
 import UIKit
+import Foundation
+import AVFoundation
+import MediaPlayer
 
-public class KeyCatcherPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "key_catcher", binaryMessenger: registrar.messenger())
-    let instance = KeyCatcherPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+public class KeyCatcherPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+    private let audioSession: AVAudioSession = AVAudioSession.sharedInstance()
+    private let notification: NotificationCenter = NotificationCenter.default
+    private var eventSink: FlutterEventSink?
+    private var isObserving: Bool = false
+    private let volumeKey: String = "outputVolume"
+    private var volumeView: MPVolumeView?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let methodChannel = FlutterMethodChannel(name: "key_catcher_method", binaryMessenger: registrar.messenger())
+        let eventChannel = FlutterEventChannel(name: "key_catcher_event", binaryMessenger: registrar.messenger())
+        let instance = KeyCatcherPlugin()
+        registrar.addMethodCallDelegate(instance, channel: methodChannel)
+        eventChannel.setStreamHandler(instance)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "init":
+            registerVolumeObserver()
+            result(true)
+        case "dispose":
+            removeVolumeObserver()
+            result(true)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        eventSink = events
+        return nil
+    }
+    
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        eventSink = nil
+        return nil
+    }
+    
+    private func registerVolumeObserver() {
+        audioSessionObserver()
+        notification.addObserver(
+            self,
+            selector: #selector(audioSessionObserver),
+            name: UIApplication.didBecomeActiveNotification,
+            object: nil)
+    }
+    
+    @objc func audioSessionObserver(){
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.playback)
+            try audioSession.setActive(true)
+            if !isObserving {
+                volumeView = MPVolumeView(frame: CGRect.zero)
+                if let volumeView = volumeView {
+                    UIApplication.shared.windows.first?.addSubview(volumeView)
+                    UIApplication.shared.windows.first?.sendSubviewToBack(volumeView)
+                }
+                audioSession.addObserver(self,
+                                         forKeyPath: volumeKey,
+                                         options: .new.union(.old),
+                                         context: nil)
+
+                isObserving = true
+            }
+        } catch {
+            print("Volume Controller Listener occurred error.")
+        }
+    }
+    
+    private func removeVolumeObserver() {
+        audioSession.removeObserver(self,
+                                    forKeyPath: volumeKey)
+        if let volumeView = volumeView {
+            volumeView.removeFromSuperview()
+        }
+        volumeView = nil
+        isObserving = false
+    }
+    
+    override public func observeValue(forKeyPath keyPath: String?,
+                                      of object: Any?,
+                                      change: [NSKeyValueChangeKey: Any]?,
+                                      context: UnsafeMutableRawPointer?) {
+        if keyPath == volumeKey {
+            let value = change?[NSKeyValueChangeKey.newKey] as? Float
+            if(value != 0.5123) {
+                eventSink?(true)
+                if let view = volumeView?.subviews.first(where: { $0 is UISlider }) as? UISlider
+                {
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
+                        view.value = 0.5123
+                    }
+                }
+            }
+        }
+    }
+
 }
